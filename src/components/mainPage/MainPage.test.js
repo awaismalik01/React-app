b593f34fb47508bfec5d9b9016c0d5e6b9557dd0
Awais Ahmed Malik <mawais_malik@hotmail.com>
2024-07-27 15:27:42 -0400

    Change Test

--- src/components/mainPage/MainPage.test.js
+++ src/components/mainPage/MainPage.test.js
@@ -4,13 +4,17 @@ import MainPage from "./MainPage";
 describe("Main Page Component Test", () => {
   test("render MainPage true", () => {
     render(<MainPage condition={1} />);
-    const linkElement = screen.getByText(/Main Page Compoent: First Condition/i);
+    const linkElement = screen.getByText(
+      /Main Page Compoent: First Condition/i
+    );
     expect(linkElement).toBeInTheDocument();
   });
 
   test("render MainPage false", () => {
     render(<MainPage condition={0} />);
-    const linkElement = screen.getByText(/Main Page Compoent: Second Condition/i);
-    expect(linkElement).toBeInTheDocument();
+    const linkElement = screen.getByText(
+      /Main Page Compoent: Second Condition/i
+    );
+    expect(linkElement).toBeDisabled();
   });
 });
