db13c7c57cc9b9dfc3bec7ae792c842761067954
Awais Ahmed Malik <mawais_malik@hotmail.com>
2024-07-29 17:25:29 -0400

    Github CI action

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-describe("Main Page Component Test", () => {
+describe("App Component Test", () => {
   test("renders learn react link", () => {
     render(<App />);
     const linkElement = screen.getByText(/learn react/i);
